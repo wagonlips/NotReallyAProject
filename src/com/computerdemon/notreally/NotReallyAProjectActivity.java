eb26314a68bf8e34eaf3ad1c138b4f5daf505a6d
Sean Camden <sean@seancamden.com>
2011-07-13 20:48:03 -0700

    Random minor change.
    "Hi There!"

--- src/com/computerdemon/notreally/NotReallyAProjectActivity.java
+++ src/com/computerdemon/notreally/NotReallyAProjectActivity.java
@@ -9,5 +9,6 @@ public class NotReallyAProjectActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        /* Hi there! */
     }
 }
